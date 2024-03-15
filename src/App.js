05b33e82e2f6b9ad7d7b2914a911ddc4c33d42a4
rohit-vns <rohitks.vns@gmail.com>
2024-03-16 03:56:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "./App.js";
 
 function App() {
+  const [value, setValue] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="calculator">
+        <input type="text" value={value} placeholder="0" />
+      </div>
+      <div>
+        <input
+          type="button"
+          className="button operator"
+          value="AC"
+          onClick={(e) => setValue("")}
+        />
+        <input
+          type="button"
+          className="button operator"
+          value="DEL"
+          onClick={(e) => setValue(value.slice(0, -1))}
+        />
+        <input
+          type="button"
+          className="button operator"
+          value="C"
+          onClick={(e) => setValue("")}
+        />
+        <input
+          type="button"
+          className="button operator"
+          value="/"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="button"
+          className="button"
+          value="7"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button"
+          value="8"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button"
+          value="9"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button operator"
+          value="*"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="button"
+          className="button"
+          value="4"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button"
+          value="5"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button"
+          value="6"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button operator"
+          value="+"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="button"
+          className="button"
+          value="1"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button"
+          value="2"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button"
+          value="3"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button operator"
+          value="-"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="button"
+          className="button"
+          value="00"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button"
+          value="0"
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          className="button"
+          value="."
+          onClick={(e) => setValue(value + e.target.value)}
+        />
+        <input
+          type="button"
+          value="="
+          className="button equalbtn"
+          // eslint-disable-next-line no-eval
+          onClick={(e) => setValue(eval(value))}
+        />
+      </div>
     </div>
   );
 }
